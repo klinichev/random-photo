60d267a42eb0fadbf717bd428bfc125e3878f632
arsenii_klinichev <arsenklinichev@gmail.com>
2022-11-08 22:03:32 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,58 @@
+(function () {
+    const pictures = [
+        {
+            id: 0,
+            uri: '../img/asha.jpg',
+            desc: 'Аша, Челябинская область, июнь 2022'
+        },
+        {
+            id: 1,
+            uri: '../img/gorokhovets.jpg',
+            desc: 'Гороховец, Владимирская область, май 2022'
+        },
+        {
+            id: 2,
+            uri: '../img/izhevsk.jpg',
+            desc: 'Ижевск, Удмуртия, май 2021'
+        },
+        {
+            id: 3,
+            uri: '../img/sim.jpg',
+            desc: 'Сим, Челябинская область, июнь 2022'
+        },
+        {
+            id: 4,
+            uri: '../img/torzhok.jpg',
+            desc: 'Торжок, Тверская область, июнь 2022'
+        },
+        {
+            id: 5,
+            uri: '../img/ufa.jpg',
+            desc: 'Уфа, Башкортостан, июнь 2022'
+        },
+        {
+            id: 6,
+            uri: '../img/ufa-2.jpg',
+            desc: 'Уфа, Башкортостан, июнь 2022'
+        },
+        {
+            id: 7,
+            uri: '../img/ufa-3.jpg',
+            desc: 'Уфа, Башкортостан, июнь 2022'
+        },
+        {
+            id: 8,
+            uri: '../img/zvenigorod.jpg',
+            desc: 'Звенигород, Московская область, май 2018'
+        },
+    ];
+  
+    function loadPicture(params) {
+
+    }
+  
+    document.addEventListener('DOMContentLoaded', function () {
+      loadPicture(document.getElementById('index'));
+    });
+  })();
+  
